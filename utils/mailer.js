a8d0ec6b90253ba3f591b823263bcfc91edb84d3
Phani Murari <imphanimurari1@gmail.com>
2024-08-13 09:47:39 +0530

    Fixed-issue-with-mail-notification

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,18 +1,21 @@
 const nodemailer = require('nodemailer');
 
+require('dotenv').config();
+
+
 // Configure the email transporter
 const transporter = nodemailer.createTransport({
-  service: 'Gmail', // You can use other services like Yahoo, Outlook, etc.
+  service: 'Gmail', // You can change this to 'Yahoo', 'Outlook', etc., depending on your provider
   auth: {
-    user: process.env.EMAIL_USER, // Your email
-    pass: process.env.EMAIL_PASS, // Your email password or app password
+    user: process.env.EMAIL_USER, // Your email address from the .env file
+    pass: process.env.EMAIL_PASS, // Your email password or app password from the .env file
   },
 });
 
 const sendMail = (to, subject, text) => {
   const mailOptions = {
     from: process.env.EMAIL_USER, // sender address
-    to, // list of receivers
+    to : ["test1@yopmail.com"], // list of receivers
     subject, // Subject line
     text, // plain text body
   };
